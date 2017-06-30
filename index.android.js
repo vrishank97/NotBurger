d95b9a4a554fbebab3660a13bb1275450020ff0d
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-07-01 00:45:19 +0530

    using Example as base

--- index.android.js
+++ index.android.js
@@ -1,54 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import Example from './Example';
 
-export default class NotBurger extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to NotBurger!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-//AppRegistry.registerComponent('NotBurger', () => NotBurger);
-//AppRegistry.registerComponent('LotsOfGreetings', () => LotsOfGreetings);
+AppRegistry.registerComponent('Example', () => Example);
